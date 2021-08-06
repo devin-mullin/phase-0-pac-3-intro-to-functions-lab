46eba7b15cf63b22dcd246ec94e62200f1c8251a
Devin Mullin <devin.mullin90@gmail.com>
2021-08-06 09:14:14 -0500

    i'm finished!

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string)
+    console.log(string.toUpperCase())
+    string === string.toUpperCase
+}
+
+function logWhisper(string) {
+    console.log(string)
+    console.log(string.toLowerCase())
+    string === string.toLowerCase
+}
+
+function sayHiToGrandma(string) {
+    return console.log("I can't hear you!")
+    if whisper;
+    return console.log("YES INDEED!")
+    if shout;
+    return console.log("I love you, too.")
+    if string === "I love you, Grandma."()
+}S
